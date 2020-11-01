b245291cdd006471258f3cd4c49f6d9929c2b030
Trista-W <31838961+Trista-W@users.noreply.github.com>
2020-11-01 16:44:18 -0500

    Update Kmeans.py

--- Kmeans.py
+++ Kmeans.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-image = cv2.imread('C:/Users/Trista/PycharmProjects/Computer Vision Projects/segmentation_project/images/bunny.bmp')
+image = cv2.imread('images/bunny.bmp')
 # cv2.imshow('image', image)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
